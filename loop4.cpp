3223833151bfd83ee2a16e22008eb9e67e09be5a
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-05 17:03:00 +0530

    Add files via upload

---
+++ loop4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout << "print all even numbers from 1 to 100";
+cin >> n;
+int i=2;
+while(i<=100)
+{
+cout << i<<endl;
+i=i+2;
+}
+return 0;
+} 
